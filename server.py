c6a2d2734261a6f318a76b9b55e7195747894095
kimalysha93 <kimalysha93@gmail.com>
2025-02-04 14:57:20 -0500

    server.py pylint

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
-# Import Flask, render_template, request from the flask pramework package:
+'''
+Executing this function initiates the application of emotion
+detection to be executed over the Flask channel and deployed on
+localhost:5000.
+'''
 from flask import Flask, render_template, request
-
-# Import the sentiment_analyzer function from the package created:
 from EmotionDetection.emotion_detection import emotion_detector
 
 #Initiate the flask app:
@@ -9,6 +11,13 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    '''
+    This code receives the text from the HTML interface and 
+    runs emotion detection over it using emotion_detection()
+    function. The output returned shows the emotions and the 
+    dominant emotion for the provided text.
+    '''
+
     # retrieve text
     text_to_analyze = request.args.get('textToAnalyze')
 
@@ -22,11 +31,24 @@ def sent_detector():
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
+    # Check if the dominant emotion is None, indicating an error or invalid input
+    if dominant_emotion is None:
+        return "Invalid text! Try again."
+
+    return (f"For the given statement, the system response is 'anger': {anger}, "
+    f"'disgust': {disgust}, "
+    f"'fear': {fear}, "
+    f"'joy': {joy} "
+    f"and 'sadness': {sadness}. "
+    f"The dominant emotion is {dominant_emotion}.")
 
 
 @app.route("/")
 def render_index_page():
+    '''
+    This function initiates the rendering of the main application
+    page over the Flask channel
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
